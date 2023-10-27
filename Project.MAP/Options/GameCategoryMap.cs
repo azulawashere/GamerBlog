2a13fce9f213db4c9b93d9d8da7b847a13256fe5
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-10-27 17:42:30 +0300

    Map katmanındaki optionsa gerekli map classları eklenip düzenlendi.

---
+++ Project.MAP/Options/GameCategoryMap.cs
@@ -0,0 +1,24 @@
+﻿using Project.ENTITES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public class GameCategoryMap:BaseMap<GameCategory>
+    {
+        public GameCategoryMap()
+        {
+           
+            //İlişki Ayarlamaları
+            Ignore(x => x.ID);
+            HasKey(x => new
+            {
+                x.CategoryID,
+                x.GameID
+            });
+        }
+    }
+}
