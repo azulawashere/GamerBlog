8e41acb71e76ae2e7bf6a06aba797a06b67a4a06
Mehmedzcann <mehmedzcann@gmail.com>
2023-10-30 14:59:07 +0300

    MainPage dizayn yapıldı.

--- Project.WinUI/MainPage.Designer.cs
+++ Project.WinUI/MainPage.Designer.cs
@@ -28,177 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cmbBegenilenOyun = new System.Windows.Forms.ComboBox();
-            this.lblTercihEdilenOyun = new System.Windows.Forms.Label();
-            this.lblKategori = new System.Windows.Forms.Label();
-            this.cmbKategori = new System.Windows.Forms.ComboBox();
-            this.txtAra = new System.Windows.Forms.TextBox();
-            this.lblBaslik = new System.Windows.Forms.Label();
-            this.cmbBegenilenEdilenOyun = new System.Windows.Forms.ComboBox();
-            this.cmbBegenilenOyunlar = new System.Windows.Forms.ComboBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
             this.btnKayitOl = new System.Windows.Forms.Button();
             this.btnKullanici = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.pctFoto = new System.Windows.Forms.PictureBox();
+            this.cmbBegenilenOyunlar = new System.Windows.Forms.ComboBox();
+            this.lblBaslik = new System.Windows.Forms.Label();
+            this.lblBegenilenOyun = new System.Windows.Forms.Label();
+            this.lblOyunKategori = new System.Windows.Forms.Label();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
             this.btnAra = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFoto)).BeginInit();
             this.SuspendLayout();
-            // 
-            // cmbBegenilenOyun
-            // 
-            this.cmbBegenilenOyun.FormattingEnabled = true;
-            this.cmbBegenilenOyun.Location = new System.Drawing.Point(602, 428);
-            this.cmbBegenilenOyun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.cmbBegenilenOyun.Name = "cmbBegenilenOyun";
-            this.cmbBegenilenOyun.Size = new System.Drawing.Size(160, 24);
-            this.cmbBegenilenOyun.TabIndex = 15;
-            // 
-            // lblTercihEdilenOyun
-            // 
-            this.lblTercihEdilenOyun.AutoSize = true;
-            this.lblTercihEdilenOyun.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblTercihEdilenOyun.Location = new System.Drawing.Point(442, 311);
-            this.lblTercihEdilenOyun.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblTercihEdilenOyun.Name = "lblTercihEdilenOyun";
-            this.lblTercihEdilenOyun.Size = new System.Drawing.Size(320, 29);
-            this.lblTercihEdilenOyun.TabIndex = 14;
-            this.lblTercihEdilenOyun.Text = "En Çok Beğenilen Oyunlar";
-            // 
-            // lblKategori
-            // 
-            this.lblKategori.AutoSize = true;
-            this.lblKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblKategori.Location = new System.Drawing.Point(13, 311);
-            this.lblKategori.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblKategori.Name = "lblKategori";
-            this.lblKategori.Size = new System.Drawing.Size(217, 29);
-            this.lblKategori.TabIndex = 13;
-            this.lblKategori.Text = "Oyun Kategorileri";
-            // 
-            // cmbKategori
-            // 
-            this.cmbKategori.FormattingEnabled = true;
-            this.cmbKategori.Location = new System.Drawing.Point(108, 368);
-            this.cmbKategori.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.cmbKategori.Name = "cmbKategori";
-            this.cmbKategori.Size = new System.Drawing.Size(160, 24);
-            this.cmbKategori.TabIndex = 12;
-            // 
-            // txtAra
-            // 
-            this.txtAra.Location = new System.Drawing.Point(195, 187);
-            this.txtAra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtAra.Multiline = true;
-            this.txtAra.Name = "txtAra";
-            this.txtAra.Size = new System.Drawing.Size(1224, 36);
-            this.txtAra.TabIndex = 11;
-            // 
-            // lblBaslik
-            // 
-            this.lblBaslik.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lblBaslik.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblBaslik.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblBaslik.Location = new System.Drawing.Point(394, 68);
-            this.lblBaslik.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblBaslik.Name = "lblBaslik";
-            this.lblBaslik.Size = new System.Drawing.Size(399, 55);
-            this.lblBaslik.TabIndex = 10;
-            this.lblBaslik.Text = "TakeBirGaming";
-            // 
-            // cmbBegenilenEdilenOyun
-            // 
-            this.cmbBegenilenEdilenOyun.FormattingEnabled = true;
-            this.cmbBegenilenEdilenOyun.Location = new System.Drawing.Point(350, 344);
-            this.cmbBegenilenEdilenOyun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.cmbBegenilenEdilenOyun.Name = "cmbBegenilenEdilenOyun";
-            this.cmbBegenilenEdilenOyun.Size = new System.Drawing.Size(160, 24);
-            this.cmbBegenilenEdilenOyun.TabIndex = 16;
-            // 
-            // cmbBegenilenOyunlar
-            // 
-            this.cmbBegenilenOyunlar.FormattingEnabled = true;
-            this.cmbBegenilenOyunlar.Location = new System.Drawing.Point(350, 376);
-            this.cmbBegenilenOyunlar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.cmbBegenilenOyunlar.Name = "cmbBegenilenOyunlar";
-            this.cmbBegenilenOyunlar.Size = new System.Drawing.Size(160, 24);
-            this.cmbBegenilenOyunlar.TabIndex = 17;
             // 
             // btnKayitOl
             // 
-            this.btnKayitOl.Location = new System.Drawing.Point(1987, 267);
-            this.btnKayitOl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKayitOl.Location = new System.Drawing.Point(1490, 217);
             this.btnKayitOl.Name = "btnKayitOl";
-            this.btnKayitOl.Size = new System.Drawing.Size(100, 28);
+            this.btnKayitOl.Size = new System.Drawing.Size(75, 23);
             this.btnKayitOl.TabIndex = 19;
             this.btnKayitOl.Text = "Kayıt Ol";
             this.btnKayitOl.UseVisualStyleBackColor = true;
             // 
             // btnKullanici
             // 
-            this.btnKullanici.Location = new System.Drawing.Point(1869, 267);
-            this.btnKullanici.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKullanici.Location = new System.Drawing.Point(1402, 217);
             this.btnKullanici.Name = "btnKullanici";
-            this.btnKullanici.Size = new System.Drawing.Size(109, 28);
+            this.btnKullanici.Size = new System.Drawing.Size(82, 23);
             this.btnKullanici.TabIndex = 18;
             this.btnKullanici.Text = "Kullanıcı Giriş";
             this.btnKullanici.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // pctFoto
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(-9, 551);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "label2";
+            this.pctFoto.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pctFoto.BackgroundImage")));
+            this.pctFoto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pctFoto.Location = new System.Drawing.Point(79, 237);
+            this.pctFoto.Name = "pctFoto";
+            this.pctFoto.Size = new System.Drawing.Size(190, 121);
+            this.pctFoto.TabIndex = 27;
+            this.pctFoto.TabStop = false;
+            this.pctFoto.Visible = false;
+            // 
+            // cmbBegenilenOyunlar
+            // 
+            this.cmbBegenilenOyunlar.FormattingEnabled = true;
+            this.cmbBegenilenOyunlar.Location = new System.Drawing.Point(326, 165);
+            this.cmbBegenilenOyunlar.Name = "cmbBegenilenOyunlar";
+            this.cmbBegenilenOyunlar.Size = new System.Drawing.Size(121, 21);
+            this.cmbBegenilenOyunlar.TabIndex = 26;
+            this.cmbBegenilenOyunlar.SelectedIndexChanged += new System.EventHandler(this.cmbBegenilenOyunlar_SelectedIndexChanged);
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblBaslik.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBaslik.Location = new System.Drawing.Point(73, 19);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(243, 33);
+            this.lblBaslik.TabIndex = 25;
+            this.lblBaslik.Text = "TakeBirGaming";
+            // 
+            // lblBegenilenOyun
+            // 
+            this.lblBegenilenOyun.AutoSize = true;
+            this.lblBegenilenOyun.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lblBegenilenOyun.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBegenilenOyun.Location = new System.Drawing.Point(322, 121);
+            this.lblBegenilenOyun.Name = "lblBegenilenOyun";
+            this.lblBegenilenOyun.Size = new System.Drawing.Size(258, 24);
+            this.lblBegenilenOyun.TabIndex = 24;
+            this.lblBegenilenOyun.Text = "En Çok Beğenilen Oyunlar";
+            // 
+            // lblOyunKategori
+            // 
+            this.lblOyunKategori.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lblOyunKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOyunKategori.Location = new System.Drawing.Point(87, 121);
+            this.lblOyunKategori.Name = "lblOyunKategori";
+            this.lblOyunKategori.Size = new System.Drawing.Size(182, 24);
+            this.lblOyunKategori.TabIndex = 23;
+            this.lblOyunKategori.Text = "Oyun Kategorileri";
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(91, 165);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(121, 21);
+            this.cmbKategori.TabIndex = 22;
+            this.cmbKategori.SelectedIndexChanged += new System.EventHandler(this.cmbKategori_SelectedIndexChanged);
             // 
             // btnAra
             // 
+            this.btnAra.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAra.BackgroundImage")));
             this.btnAra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnAra.Location = new System.Drawing.Point(1480, 341);
-            this.btnAra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAra.Location = new System.Drawing.Point(941, 83);
             this.btnAra.Name = "btnAra";
-            this.btnAra.Size = new System.Drawing.Size(75, 37);
+            this.btnAra.Size = new System.Drawing.Size(39, 21);
             this.btnAra.TabIndex = 21;
             this.btnAra.UseVisualStyleBackColor = true;
             // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(79, 84);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(863, 20);
+            this.txtAra.TabIndex = 20;
+            // 
             // MainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 831);
-            this.Controls.Add(this.cmbBegenilenOyun);
-            this.Controls.Add(this.lblTercihEdilenOyun);
-            this.Controls.Add(this.lblKategori);
-            this.Controls.Add(this.cmbKategori);
-            this.Controls.Add(this.txtAra);
-            this.Controls.Add(this.lblBaslik);
-            this.Controls.Add(this.cmbBegenilenEdilenOyun);
+            this.BackgroundImage = global::Project.WinUI.Properties.Resources.darth_vader_sith_star_wars_dark_wallpaper_preview;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1306, 675);
+            this.Controls.Add(this.pctFoto);
             this.Controls.Add(this.cmbBegenilenOyunlar);
+            this.Controls.Add(this.lblBaslik);
+            this.Controls.Add(this.lblBegenilenOyun);
+            this.Controls.Add(this.lblOyunKategori);
+            this.Controls.Add(this.cmbKategori);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.txtAra);
             this.Controls.Add(this.btnKayitOl);
             this.Controls.Add(this.btnKullanici);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnAra);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainPage";
             this.Text = "MainPage";
+            this.Load += new System.EventHandler(this.MainPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctFoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox cmbBegenilenOyun;
-        private System.Windows.Forms.Label lblTercihEdilenOyun;
-        private System.Windows.Forms.Label lblKategori;
-        private System.Windows.Forms.ComboBox cmbKategori;
-        private System.Windows.Forms.TextBox txtAra;
-        private System.Windows.Forms.Label lblBaslik;
-        private System.Windows.Forms.ComboBox cmbBegenilenEdilenOyun;
-        private System.Windows.Forms.ComboBox cmbBegenilenOyunlar;
         private System.Windows.Forms.Button btnKayitOl;
         private System.Windows.Forms.Button btnKullanici;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pctFoto;
+        private System.Windows.Forms.ComboBox cmbBegenilenOyunlar;
+        private System.Windows.Forms.Label lblBaslik;
+        private System.Windows.Forms.Label lblBegenilenOyun;
+        private System.Windows.Forms.Label lblOyunKategori;
+        private System.Windows.Forms.ComboBox cmbKategori;
         private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAra;
     }
 }
--- Project.WinUI/MainPage.cs
+++ Project.WinUI/MainPage.cs
@@ -16,5 +16,52 @@ namespace Project.WinUI
         {
             InitializeComponent();
         }
+        string[] veriler = { "Battlefront3", "csgo", "Fifa24" };
+        string[] begenilenOyun = { "Battlefront3", "Fifa24" };
+        
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+            cmbKategori.Items.AddRange(veriler);
+            cmbBegenilenOyunlar.Items.AddRange(begenilenOyun);
+
+        }
+        private void cmbKategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pctFoto.Visible = false;
+            if (cmbKategori.SelectedItem as string == "Battlefront3")
+            {
+                if (cmbKategori.SelectedItem == veriler)
+                {
+                    pctFoto.Visible = false;
+                }
+                else
+                {
+                    pctFoto.Visible = true;
+                }
+            }
+
+        }
+
+        private void cmbBegenilenOyunlar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbBegenilenOyunlar.SelectedItem as string == "Battlefront3")
+            {
+                this.BackgroundImage = Properties.Resources._90cd46ae29323a1f31d55900c3a2436d;
+
+
+            }
+            else if (cmbBegenilenOyunlar.SelectedItem as string == "Fifa24")
+            {
+                this.BackgroundImage = Properties.Resources.ronaldo_nazario_real_madrid_uowrhe3shq301vp41b5ha5ct4;
+
+            }
+            else
+            {
+                this.BackgroundImage = Properties.Resources.darth_vader_sith_star_wars_dark_wallpaper_preview;
+            }
+            pctFoto.Visible = false;
+
+        }
     }
 }
