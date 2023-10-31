bcd4a968527cf15b03d426806313ee4508d3ffa4
MuratcanErn <hmuratcaneren@hotmail.com>
2023-10-31 23:05:22 +0300

    ref

--- Project.WinUI/MainPage.cs
+++ Project.WinUI/MainPage.cs
@@ -19,6 +19,8 @@ namespace Project.WinUI
         AppUsersRepository _appUsersRepository;
         CommentRepository _commentRepository;
         GameRepository _gameRepository;
+     
+   
 
         public MainPage(AppUser user)
         {
@@ -141,5 +143,9 @@ namespace Project.WinUI
         {
             MainPage_Load(sender,e);
         }
+
+     
+
+       
     }
 }
